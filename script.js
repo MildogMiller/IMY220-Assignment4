5c93ff7aa3bbb5b1ab849c0a098cec71dc082803
Adir Miller <adirmildog@gmail.com>
2021-09-27 10:47:43 +0200

    Added image drag and drop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
+    //handle update logic
     $("button").on('click', function () {
-        if($(this).hasClass("update")){
+        if ($(this).hasClass("update")) {
             $(this).parent().children().not("button").toggle();
             $(this).parent().children("span").html($(this).parent().children("input").val());
             $(this).parent().children("input").remove();
@@ -12,4 +13,16 @@ $(document).ready(() => {
             $(this).parent().children("button").toggleClass("update").html("Update");
         }
     });
+
+    //since we cant add more classes just using the col
+    $(".col").append("<input type='file' class='form-control' name='img' id='img' accept='image/png,image/jpg'/>");
+    $("#img").change(function () {
+        if($('#img').get(0).files[0]){
+            var read=new FileReader();
+            read.onload=()=>$("img").attr("src",read.result);
+            read.readAsDataURL($('#img').get(0).files[0]);
+        }
+    });
+
+
 });
